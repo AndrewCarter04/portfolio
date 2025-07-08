bc143115c64af93fa35eaf633875cdf9d45c2090
Andrew Carter <68480855+AndrewCarter04@users.noreply.github.com>
2025-07-08 23:07:05 +0100

    Home page complete

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
 
 function Navigation() {
   return (
@@ -15,12 +16,11 @@ function Navigation() {
       }}
     >
       <Container>
-        <Navbar.Brand as={NavLink} to="/">
-          Andrew Carter
-        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
+
+          {/* Left side nav links */}
+          <Nav className="me-auto">
             <Nav.Link as={NavLink} to="/" end>
               Home
             </Nav.Link>
@@ -34,6 +34,20 @@ function Navigation() {
               Awards
             </Nav.Link>
           </Nav>
+
+          {/* Right side social icons */}
+          <Nav className="ms-auto">
+            <Nav.Link href="https://github.com/AndrewCarter04" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+              <FaGithub size={22} />
+            </Nav.Link>
+            <Nav.Link href="https://www.linkedin.com/in/andrewcarter04/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+              <FaLinkedin size={22} />
+            </Nav.Link>
+            <Nav.Link href="https://instagram.com/andrew.carter04" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+              <FaInstagram size={22} />
+            </Nav.Link>
+          </Nav>
+
         </Navbar.Collapse>
       </Container>
     </Navbar>
